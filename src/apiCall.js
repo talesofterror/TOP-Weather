8639acbe6ace0b663b13a885bb6526a849a394e3
mike <mich.rivera84@gmail.com>
2025-09-13 11:37:29 -0400

    creating the custom report object from the response. also added some error handling

--- src/apiCall.js
+++ src/apiCall.js
@@ -1,11 +1,48 @@
+import testData from "./assets/weatherRefData.json"
 
 let weatherRequestUrl = (query) => `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/`
 	+ query
 	+ `?unitGroup=us&key=GKRUZWJ6T7MMYA9ZMP78HA9DL&contentType=json`
 
-export async function getReport (query) {
-	let response = await fetch(weatherRequestUrl(query))
-	return await response.json()
+export async function getReport (query, testRun = false) {
+	let response
+	let json
+	try {
+		if (!testRun) {
+			response = await fetch(weatherRequestUrl(query))
+		}
+		json = testRun? testData : response.json()
+		return {
+			currentConditions: {
+				icon: json.currentConditions.icon, 
+				temp: json.currentConditions.temp,
+				tempmax: json.currentConditions.tempmax,
+				tempmin: json.currentConditions.tempmin,
+				conditions: json.currentConditions.conditions,
+				humidity: json.currentConditions.humidity,
+			},
+			days: [
+				{
+					temp: json.days[1].temp,
+					icon: json.days[1].icon
+				},
+				{
+					temp: json.days[2].temp,
+					icon: json.days[2].icon
+				}
+			]	
+		}
+	} catch (e) {
+		console.log("response:")
+		console.log(response)
+		console.log("json:")
+		console.log(json)
+		console.log(json.days[0].temp)
+		console.log(json.days[1].temp)
+		console.log("error: ")
+		throw new Error(e)
+	}
+
 }
 
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import {getReport} from "./apiCall.js"
 
-console.log(await getReport("Philadelphia"))
+console.log(await getReport("Philadelphia", true))
